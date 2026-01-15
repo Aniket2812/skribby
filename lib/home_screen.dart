04c8391b23ce95591bb04c4b27c52cc6811e3be9
Aniket2812 <aniketyadav982@gmail.com>
2026-01-16 01:21:14 +0530

    home screen made

---
+++ lib/home_screen.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          const Text(
+            "Create/Join a room to play!",
+            style: TextStyle(color: Colors.black, fontSize: 24),
+          ),
+          SizedBox(height: MediaQuery.of(context).size.height * 0.1),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              ElevatedButton(
+                onPressed: () {},
+                style: ButtonStyle(
+                  backgroundColor: MaterialStateProperty.all(Colors.blue),
+                  textStyle: MaterialStateProperty.all(
+                    TextStyle(color: Colors.white),
+                  ),
+                  minimumSize: MaterialStateProperty.all(
+                    Size(MediaQuery.of(context).size.width / 2.5, 50),
+                  ),
+                ),
+                child: const Text(
+                  "Create",
+                  style: TextStyle(
+                    fontSize: 16
+                  ),
+                ),
+              ),
+              ElevatedButton(
+                onPressed: () {},
+                style: ButtonStyle(
+                  backgroundColor: MaterialStateProperty.all(Colors.blue),
+                  textStyle: MaterialStateProperty.all(
+                    TextStyle(color: Colors.white),
+                  ),
+                  minimumSize: MaterialStateProperty.all(
+                    Size(MediaQuery.of(context).size.width / 2.5, 50),
+                  ),
+                ),
+                child: const Text(
+                  "Join",
+                  style: TextStyle(
+                    fontSize: 16
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:skribby/home_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -15,7 +16,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const Text("test")
+      home: const HomeScreen()
     );
   }
 }
