51d696d92eda662d61981ce112e82c37d5f9e403
Aniket2812 <aniketyadav982@gmail.com>
2026-01-16 03:58:58 +0530

    paint screen and socket setup

---
+++ server/index.js
@@ -0,0 +1,24 @@
+const express = require("express");
+var http = require("http");
+const app = express();
+const port = process.env.PORT || 3000
+var server = http.createServer(app);
+const mongoose = require("mongoose");
+
+var io = require("socket.io")(server);
+
+//middleware
+app.use(express.json())
+
+//connect to mongodb
+const DB = "mongodb+srv://aniket:aniket%40123@skribby.xdp1p2e.mongodb.net/skribby?retryWrites=true&w=majority";
+
+mongoose.connect(DB).then(() => {
+    console.log('Connection Succesful!');
+}).catch((e) => {
+    console.log(e);
+})
+
+server.listen(port, "0.0.0.0", () => {
+    console.log('Server started and running on port ' + port);
+})
